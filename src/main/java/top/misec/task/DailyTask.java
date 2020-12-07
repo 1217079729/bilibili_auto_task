a45e2c8c1ae5747a0bd43a342c1d5f5bc41bf558
lensu <1217079729@qq.com>
2020-12-07 13:53:19 +0800

    update

---
+++ src/main/java/top/misec/task/DailyTask.java
@@ -0,0 +1,77 @@
+package top.misec.task;
+
+import com.google.gson.JsonObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import top.misec.apiquery.ApiList;
+import top.misec.utils.HttpUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import static top.misec.task.TaskInfoHolder.calculateUpgradeDays;
+import static top.misec.task.TaskInfoHolder.statusCodeStr;
+
+/**
+ * @author @JunzhouLiu @Kurenai
+ * @create 2020/10/11 20:44
+ */
+public class DailyTask {
+    static Logger logger = (Logger) LogManager.getLogger(DailyTask.class.getName());
+
+    private final List<Task> dailyTasks =
+            Arrays.asList(new UserCheck(), new VideoWatch(), new MangaSign(), new CoinAdd(), new Silver2coin(), new LiveCheckin(), new ChargeMe(), new GetMangaVipReward());
+
+
+    public void doDailyTask() {
+        try {
+            printTime();
+            logger.debug("任务启动中");
+            for (Task task : dailyTasks) {
+                logger.info("-----{}开始-----", task.getName());
+                task.run();
+                logger.info("-----任务结束-----\n");
+                Random random = new Random();
+                int sleepTime = (int) ((random.nextDouble() + 0.5) * 3000);
+                logger.info("随机暂停{}ms", sleepTime);
+                Thread.sleep(sleepTime);
+            }
+            logger.info("本日任务已全部执行完毕");
+            calculateUpgradeDays();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            ServerPush.doServerPush();
+        }
+    }
+
+    /**
+     * @return jsonObject 返回status对象，包含{"login":true,"watch":true,"coins":50,
+     * "share":true,"email":true,"tel":true,"safe_question":true,"identify_card":false}
+     * @author @srcrs
+     */
+    public static JsonObject getDailyTaskStatus() {
+        JsonObject jsonObject = HttpUtil.doGet(ApiList.reward);
+        int responseCode = jsonObject.get(statusCodeStr).getAsInt();
+        if (responseCode == 0) {
+            logger.info("请求本日任务完成状态成功");
+            return jsonObject.get("data").getAsJsonObject();
+        } else {
+            logger.debug(jsonObject.get("message").getAsString());
+            return HttpUtil.doGet(ApiList.reward).get("data").getAsJsonObject();
+            //偶发性请求失败，再请求一次。
+        }
+    }
+
+    private void printTime() {
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = sdf.format(d);
+        logger.info(time);
+    }
+
+}
+
---
+++ src/main/resources/README.md
@@ -0,0 +1,9 @@
+{
+  "numberOfCoins": //--5,每日投币数量,默认 5 ,为0时则不投币
+  "selectLike": //--0,投币时是否点赞，默认 0, 0：否 1：是
+  "monthEndAutoCharge": true,//--年度大会员月底是否用 B币券 给自己充电，默认 true
+  "devicePlatform": "ios",//--手机端漫画签到时的平台，建议选择你设备的平台 ，默认 ios
+  "coinAddPriority": 1,//--0：优先给热榜视频投币，1：优先给关注的up投币
+  "skipDailyTask": 0,//--是否跳过每日任务，如果需要临时关闭每日任务，此项改为1即可，开启则改为0即可
+  "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"
+}
